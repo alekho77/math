ef68b0ab1a5cd3298dbbcc8978a831d9368cc611
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-10-25 20:49:27 +0300

    Draft for calculation derivative and its unit-tests.

--- mathlib-test/mathlib-test/diff_test.cpp
+++ mathlib-test/mathlib-test/diff_test.cpp
@@ -5,7 +5,22 @@
 namespace mathlib {
 
 class diff_test_fixture : public ::testing::Test {
-protected:
+public:
+  const double x1 = 1234.0;
+
+  static double foo1(double x) {
+    return x;
+  }
+  double foo2(double x) {
+    return x;
+  }
 };
+
+TEST_F(diff_test_fixture, construct) {
+  auto d1 = make_diff(&foo1);
+  auto d2 = make_diff(&diff_test_fixture::foo2, static_cast<diff_test_fixture*>(this));
+  d1(x1);
+  d2(x1);
+}
 
 }  // namespace mathlib
--- mathlib/diff.h
+++ mathlib/diff.h
@@ -7,9 +7,60 @@
 #define MATHLIB_DIFF_H
 
 #include <limits>
+#include <functional>
+#include <type_traits>
 
 namespace mathlib {
 
+template<typename First, typename... Rest>
+struct is_floating_point_helper {
+  static constexpr bool value = std::is_floating_point<First>::value && is_floating_point_helper<Rest...>::value;
+};
+
+template<typename First>
+struct is_floating_point_helper<First> {
+  static constexpr bool value = std::is_floating_point<First>::value;
+};
+
+template<typename A, typename B, typename... Rest>
+struct is_same_helper {
+  static constexpr bool value = std::is_same<A, B>::value && is_same_helper<B, Rest...>::value;
+};
+
+template<typename A, typename B>
+struct is_same_helper<A, B> {
+  static constexpr bool value = std::is_same<A, B>::value;
+};
+
+// Partial derivative
+template<typename R, typename... Args>
+class diff {
+  static_assert(is_floating_point_helper<R, Args...>::value == true, "Differentiating of non-floating number function is not supported.");
+  static_assert(is_same_helper<R, Args...>::value == true, "Different types are not allowed.");
+  using function_t = std::function<R(Args...)>;
+public:
+  diff(const function_t& f, R eps) : func_(f), epsilon_(eps) {}
+  explicit diff(const function_t& f) : diff(f, std::numeric_limits<R>::epsilon() * (R)(10)) {}
+  diff(const diff&) = default;
+  diff(diff&&) = default;
+
+  R operator ()(Args... args) {
+    return func_(args...);
+  }
+private:
+  R epsilon_;
+  function_t func_;
+};
+
+template<typename R, typename... Args>
+typename diff<R, Args...> make_diff(R(*func)(Args...), R eps = (std::numeric_limits<R>::epsilon() * (R)(10))) {
+  return diff<R, Args...>(func, eps);
+}
+
+template<typename C, typename R, typename... Args>
+typename diff<R, Args...> make_diff(R(C::*func)(Args...), C* that, R eps = (std::numeric_limits<R>::epsilon() * (R)(10))) {
+  return diff<R, Args...>([that, func](Args... args)->R { return (that->*func)(args...); }, eps);
+}
 
 }  // namespace mathlib
 
