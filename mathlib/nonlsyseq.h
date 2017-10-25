b3a8f8637505b4eaf2c04615d64c2a585606a77b
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-10-25 17:37:51 +0300

    Added empty header file for declaring methods to solve non-linear equations.

---
+++ mathlib/nonlsyseq.h
@@ -0,0 +1,17 @@
+/*
+    Solving system of non-linear equations.
+    (c) 2017 Aleksey Khozin
+*/
+
+#ifndef MATHLIB_NONLSYSEQ_H
+#define MATHLIB_NONLSYSEQ_H
+
+#include "matrix.h"
+
+namespace mathlib {
+
+
+
+}  // namespace mathlib
+
+#endif  // MATHLIB_NONLSYSEQ_H
