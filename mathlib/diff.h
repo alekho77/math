cc23d1490602da8a9616626fee73cfcd07de5573
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-10-25 18:21:02 +0300

    Added empty header file for declaring methods to differentiate.

---
+++ mathlib/diff.h
@@ -0,0 +1,16 @@
+/*
+  Numerical differentiation.
+  (c) 2017 Aleksey Khozin
+*/
+
+#ifndef MATHLIB_DIFF_H
+#define MATHLIB_DIFF_H
+
+#include <limits>
+
+namespace mathlib {
+
+
+}  // namespace mathlib
+
+#endif  // MATHLIB_DIFF_H
