32be61a1634a24e72a8c99e7047dc67164110a26
Aleksey Khozin <ahozin@hotmail.com>
2017-10-27 00:00:17 +0300

    Working draft of derivative calculation.

--- mathlib-test/mathlib-test/diff_test.cpp
+++ mathlib-test/mathlib-test/diff_test.cpp
@@ -7,20 +7,25 @@ namespace mathlib {
 class diff_test_fixture : public ::testing::Test {
 public:
   const double x1 = 1234.0;
+  const double y1 = 2.0;
+  const double z1 = 3.0;
 
   static double foo1(double x) {
     return x;
   }
-  double foo2(double x) {
-    return x;
+  const double dfoo1_x = 1.0;
+
+  double foo2(double x, double y, double z) {
+    return x + y + z;
   }
+  const double dfoo2_y = 1.0;
 };
 
 TEST_F(diff_test_fixture, construct) {
   auto d1 = make_diff(&foo1);
   auto d2 = make_diff(&diff_test_fixture::foo2, static_cast<diff_test_fixture*>(this));
-  d1(x1);
-  d2(x1);
+  EXPECT_NEAR(dfoo1_x, d1.deriv<0>(x1), 1e-6);
+  EXPECT_NEAR(dfoo2_y, d2.deriv<1>(x1, y1, z1), 1e-6);
 }
 
 }  // namespace mathlib
--- mathlib/diff.h
+++ mathlib/diff.h
@@ -9,6 +9,7 @@
 #include <limits>
 #include <functional>
 #include <type_traits>
+#include <tuple>
 
 namespace mathlib {
 
@@ -40,25 +41,49 @@ class diff {
   using function_t = std::function<R(Args...)>;
 public:
   diff(const function_t& f, R eps) : func_(f), epsilon_(eps) {}
-  explicit diff(const function_t& f) : diff(f, std::numeric_limits<R>::epsilon() * (R)(10)) {}
+  explicit diff(const function_t& f) : diff(f, std::numeric_limits<R>::epsilon() * (R)(1000)) {}
   diff(const diff&) = default;
   diff(diff&&) = default;
 
-  R operator ()(Args... args) {
-    return func_(args...);
+  template<size_t K>
+  R deriv(Args... args) {
+    static_assert(K < sizeof...(Args), "Index of variable has to be less than number of arguments.");
+    using namespace std;
+    const auto vars = make_tuple(args...);
+    R h = (get<K>(vars) == (R)(0)) ? (epsilon_ * 1000) : (abs(get<K>(vars)) * epsilon_ * 1000);
+    R d_res = dh<K>(h, vars, index_sequence_for<Args...>{});
+    R d_eps;
+    do {
+      R d1 = d_res;
+      h /= (R)(2);
+      d_res = dh<K>(h, vars, index_sequence_for<Args...>{});
+      d_eps = (d_res == (R)(0)) ? abs(d1 - d_res) : (abs(d1 - d_res) / d_res);
+    } while (d_eps > epsilon_);
+    return d_res;
   }
 private:
-  R epsilon_;
+  template<size_t K, size_t... I>
+  R dh(R h, const std::tuple<Args...>& vars, std::index_sequence<I...>) {
+    using namespace std;
+    auto vars_left = vars;
+    get<K>(vars_left) -= h;
+    R f_left = func_(get<I>(vars_left)...);
+    auto vars_right = vars;
+    get<K>(vars_right) += h;
+    R f_right = func_(get<I>(vars_right)...);
+    return (f_right - f_left) / (2 * h);
+  }
+  const R epsilon_;
   function_t func_;
 };
 
 template<typename R, typename... Args>
-typename diff<R, Args...> make_diff(R(*func)(Args...), R eps = (std::numeric_limits<R>::epsilon() * (R)(10))) {
+typename diff<R, Args...> make_diff(R(*func)(Args...), R eps = (std::numeric_limits<R>::epsilon() * (R)(1000))) {
   return diff<R, Args...>(func, eps);
 }
 
 template<typename C, typename R, typename... Args>
-typename diff<R, Args...> make_diff(R(C::*func)(Args...), C* that, R eps = (std::numeric_limits<R>::epsilon() * (R)(10))) {
+typename diff<R, Args...> make_diff(R(C::*func)(Args...), C* that, R eps = (std::numeric_limits<R>::epsilon() * (R)(1000))) {
   return diff<R, Args...>([that, func](Args... args)->R { return (that->*func)(args...); }, eps);
 }
 
