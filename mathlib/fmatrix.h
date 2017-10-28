fdfe05e35f7b18efb40775057c58fd1ebc57f8e6
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-10-28 18:08:08 +0300

    Functional matrix has been implemented.

--- mathlib/fmatrix.h
+++ mathlib/fmatrix.h
@@ -19,17 +19,56 @@ class fmatrix;
 template<typename R, typename... Args>
 class fmatrix<R(Args...)> {
   static_assert(is_same_helper<R, Args...>::value == true, "Different types are not allowed.");
+
   using function_t = std::function<R(Args...)>;
   using data_container = std::vector<typename function_t>;
+  using const_iterator = typename data_container::const_iterator;
+  using iterator = typename data_container::iterator;
+
+  template <typename iter_t>
+  class row {
+  public:
+    row() = delete;
+    row(row&& /*r*/) = default;
+    row(const row& /*r*/) = default;
+    row(iter_t&& r) : row_begin(r) {}
+    typename iter_t::reference operator [] (size_t c) {
+      return *(row_begin + c);
+    }
+    typename iter_t::reference operator [] (size_t c) const {
+      return *(row_begin + c);
+    }
+  private:
+    iter_t row_begin;
+  };
+
 public:
   fmatrix() : fmatrix(0, 0) {}
-  fmatrix(const size_t r) : fmatrix(r, 1) {}
-  fmatrix(const size_t r, const size_t c) : rows_(r), cols_(c) {
-    data_.reserve(r * c);
+  explicit fmatrix(const size_t r) : fmatrix(r, 1) {}
+  fmatrix(const size_t r, const size_t c) : rows_(r), cols_(c), data_(r * c, typename function_t()) {}
+  fmatrix(const std::initializer_list<std::initializer_list<function_t>>& list) noexcept(false)
+    : fmatrix(list.size(), list.begin()->size()) {
+    auto iter = data_.begin();
+    for (const auto& r : list) {
+      if (r.size() == cols_) {
+        for (const auto& i : r) {
+          *iter = i;
+          ++iter;
+        }
+      } else {
+        throw std::range_error("Row lengths are different in the initialization list.");
+      }
+    }
   }
 
-  matrix<R> operator ()(Args... /*args*/) {
-    return matrix<R>();
+  matrix<R> operator ()(Args... args) const {
+    matrix<R> res{rows_, cols_};
+    for (size_t r = 0; r < rows_; r++) {
+      for (size_t c = 0; c < cols_; c++) {
+        res[r][c] = (*this)[r][c](args...);
+      }
+    }
+    return res;
   }
 
   size_t cols() const { return cols_; }  // number of columns
@@ -37,15 +76,72 @@ public:
 
   bool empty() const noexcept { return data_.empty(); }
 
+  const row<const_iterator> operator [] (size_t r) const {
+    return row<const_iterator>{data_.cbegin() + (r * cols_)};
+  }
+  row<iterator> operator [] (size_t r) {
+    return row<iterator>{data_.begin() + (r * cols_)};
+  }
+
+  fmatrix<R(Args...)>& swap_row(size_t r1, size_t r2) {
+    auto a = data_.begin() + r1 * cols_;
+    auto b = data_.begin() + r2 * cols_;
+    std::swap_ranges(a, a + cols_, b);
+    return *this;
+  }
+
 private:
   size_t rows_;
   size_t cols_;
   data_container data_;  // Array of matrix numbers
 };
 
-//typename fmatrix<R, Args...> make_fmatrix() {
-//  return fmatrix<R, Args...>();
-//}
+template <typename R, typename... Args>
+static inline fmatrix<R(Args...)> operator * (const fmatrix<R(Args...)>& m1, const fmatrix<R(Args...)>& m2) noexcept(false) {
+  if (m1.cols() == m2.rows()) {
+    fmatrix<R(Args...)> rm{m1.rows(), m2.cols()};
+    for (size_t i = 0; i < m1.rows(); i++) {
+      for (size_t j = 0; j < m2.cols(); j++) {
+        rm[i][j] = [i, j, m1, m2](Args... args)->R { 
+          R res = 0;
+          for (size_t k = 0; k < m1.cols(); k++) {
+            res += m1[i][k](args...) * m2[k][j](args...);
+          }
+          return res; };
+      }
+    }
+    return rm;
+  }
+  throw std::range_error("For matrix multiplication cols of M1 shall be equals rows of M2.");
+}
+
+template <typename R, typename... Args>
+static inline fmatrix<R(Args...)> operator + (const fmatrix<R(Args...)>& m1, const fmatrix<R(Args...)>& m2) noexcept(false) {
+  if (m1.rows() == m2.rows() && m1.cols() == m2.cols()) {
+    fmatrix<R(Args...)> rm{m1.rows(), m1.cols()};
+    for (size_t i = 0; i < m1.rows(); i++) {
+      for (size_t j = 0; j < m1.cols(); j++) {
+        rm[i][j] = [i, j, m1, m2](Args... args)->R { return m1[i][j](args...) + m2[i][j](args...); };
+      }
+    }
+    return rm;
+  }
+  throw std::range_error("For matrix addition the sizes of both matrix shall be equals.");
+}
+
+template <typename R, typename... Args>
+static inline fmatrix<R(Args...)> operator - (const fmatrix<R(Args...)>& m1, const fmatrix<R(Args...)>& m2) noexcept(false) {
+  if (m1.rows() == m2.rows() && m1.cols() == m2.cols()) {
+    fmatrix<R(Args...)> rm{m1.rows(), m1.cols()};
+    for (size_t i = 0; i < m1.rows(); i++) {
+      for (size_t j = 0; j < m1.cols(); j++) {
+        rm[i][j] = [i, j, m1, m2](Args... args)->R { return m1[i][j](args...) - m2[i][j](args...); };
+      }
+    }
+    return rm;
+  }
+  throw std::range_error("For matrix subtraction the sizes of both matrix shall be equals.");
+}
 
 }  // namespace mathlib
 
