1c8098ef6dcd7f9622bc871282841765c50f37a4
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-10-28 19:37:13 +0300

    Draft of object for solving system of non-linear equations.

---
+++ mathlib-test/mathlib-test/nonlsyseq_test.cpp
@@ -0,0 +1,20 @@
+#include "math/mathlib/nonlsyseq.h"
+
+#include <gtest/gtest.h>
+
+namespace mathlib {
+class nonlsyseq_test_fixture : public ::testing::Test {
+protected:
+};
+
+TEST_F(nonlsyseq_test_fixture, intialization) {
+  nonlinear_equations<float(float)>({[](float x) { return x; }, [](float x) { return 2*x; }, [](float x) { return 3*x; }});
+  
+  //EXPECT_THROW(linear_equations<double>({{1, 1, 1},{5, 3, 2}}, {{25},{0}}), std::exception);
+  //EXPECT_THROW(linear_equations<double>({{1, 1, 1},{5, 3, 2},{5, 3, 2},{0, 1, -1}}, {{25},{0},{6},{0}}), std::exception);
+  //EXPECT_THROW(linear_equations<double>({{1, 1, 1},{5, 3, 2},{5, 3, 2}}, {{25, 0},{0, 6},{6, 0}}), std::exception);
+  //EXPECT_NO_THROW(linear_equations<double>({{1, 1, 1},{5, 3, 2},{0, 1, -1}}, {{25},{0},{6}}));
+  //EXPECT_NO_THROW(linear_equations<double>(A, B));
+}
+
+}  // namespace mathlib
--- mathlib/nonlsyseq.h
+++ mathlib/nonlsyseq.h
@@ -6,11 +6,61 @@
 #ifndef MATHLIB_NONLSYSEQ_H
 #define MATHLIB_NONLSYSEQ_H
 
-#include "matrix.h"
+#include "fmatrix.h"
+#include "lsyseq.h"
+#include "derivative.h"
 
 namespace mathlib {
 
+template<typename F>
+class nonlinear_equations;
 
+template<typename R, typename... Args>
+class nonlinear_equations<R(Args...)> {
+  static_assert(sizeof...(Args) > 0, "Functions of the system shall have arguments.");
+  using function_t = std::function<R(Args...)>;
+  using derivative_t = derivative<R(Args...)>;
+
+public:
+  nonlinear_equations() = delete;
+  nonlinear_equations(const std::initializer_list<function_t>& list) {
+    if (list.size() == args_size) {
+      derivatives_.reserve(args_size);
+      auto fiter = list.begin();
+      for (size_t i = 0; i < args_size; i++, ++fiter) {
+        F_[i][0] = *fiter;
+        make_derivatives(*fiter, i, std::index_sequence_for<Args...>{});
+      }
+    } else {
+      throw std::invalid_argument("Number of equations shall be equal number of variables.");
+    }
+  }
+
+private:
+  template<size_t... I>
+  void make_derivatives(const function_t& fun, size_t idx, std::index_sequence<I...>) {
+    const derivative_t& deriv = derivative_t{fun};
+    derivatives_.push_back(deriv);
+    make_jacobian_row(idx, {make_derivative<I>(deriv)...});
+  }
+
+  template<size_t K>
+  function_t make_derivative(const derivative_t& deriv) {
+    return [&deriv](Args... args)->R { return deriv.diff<K>(args...); };
+  }
+
+  void make_jacobian_row(size_t idx, const std::initializer_list<function_t>& list) {
+    auto fiter = list.begin();
+    for (size_t j = 0; j < args_size; j++, ++fiter) {
+      W_[idx][j] = *fiter;
+    }
+  }
+
+  static constexpr size_t args_size = sizeof...(Args);
+  fmatrix<R(Args...)> F_ = fmatrix<R(Args...)>{args_size};  // Ñolumn-matrix for system of non-linear equations, it is assumed that each F_[i][0](args...) = 0.
+  fmatrix<R(Args...)> W_ = fmatrix<R(Args...)>{args_size, args_size};  // Jacobian
+  std::vector<derivative_t> derivatives_;
+};
 
 }  // namespace mathlib
 
