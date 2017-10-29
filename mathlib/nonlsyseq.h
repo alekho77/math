9d7bce20e39d50756b61cd29204cf77cca0a90cf
Aleksey Khozin <ahozin@hotmail.com>
2017-10-29 23:21:55 +0300

    Implementing solving system of non-linear equations.

--- mathlib/nonlsyseq.h
+++ mathlib/nonlsyseq.h
@@ -36,6 +36,45 @@ public:
     }
   }
 
+  matrix<R> solve(Args... args) const noexcept(false) {
+    return solve(transpose(matrix<R>{{args...}}), numeric_consts<R>::epsilon);
+  }
+
+  matrix<R> solve(matrix<R> x, const R epsilon) const noexcept(false) {
+    linear_equations<R> syseq = make_syseq(x, std::index_sequence_for<Args...>{});
+    matrix<R> dx = syseq.normalize().solve();
+    x += dx;
+    R eps = residual(1, x);
+    while (eps > epsilon) {
+      syseq = make_syseq(x, std::index_sequence_for<Args...>{});
+      dx = syseq.normalize().solve();
+      matrix<R> xt = x + dx;
+      R epst = residual(1, xt);
+      if (epst >= eps) {
+        break;
+      }
+      x = xt;
+      eps = epst;
+    }
+    return x;
+  }
+
+  R residual(int n, Args... args) const {
+    matrix<R> e = F_(args...);
+    R r = 0;
+    for (size_t i = 0; i < e.rows(); i++) {
+      r += powi(std::abs(e[i][0]), n);
+    }
+    return std::pow(r, static_cast<R>(1) / static_cast<R>(n));
+  }
+
+  R residual(int n, const matrix<R>& x) const {
+    if (x.rows() != sizeof...(Args)) {
+      throw std::invalid_argument("Number of results shall be equal number of variables.");
+    }
+    return residual_adapter(n, x, std::index_sequence_for<Args...>{});
+  }
+
 private:
   template<size_t... I>
   void make_derivatives(const function_t& fun, size_t idx, std::index_sequence<I...>) {
@@ -53,6 +92,16 @@ private:
     for (size_t j = 0; j < args_size; j++, ++fiter) {
       W_[idx][j] = *fiter;
     }
+  }
+
+  template<size_t... I>
+  R residual_adapter(int n, const matrix<R>& x, std::index_sequence<I...>) const {
+    return residual(n, x[I][0]...);
+  }
+
+  template<size_t... I>
+  linear_equations<R> make_syseq(const matrix<R>& x, std::index_sequence<I...>) const {
+    return linear_equations<R>(W_(x[I][0]...), -F_(x[I][0]...));
   }
 
   static constexpr size_t args_size = sizeof...(Args);
