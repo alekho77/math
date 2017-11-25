93f7e11d218095e63fa2d90d316645c2a2d21a3d
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-11-25 19:46:28 +0300

    Empty header for AN definition.

---
+++ mathlib/neuron.h
@@ -0,0 +1,13 @@
+/*
+  Object "Neuron" for artificial neural network modeling.
+  (c) 2017 Aleksey Khozin
+*/
+
+#ifndef MATHLIB_NEURON_H
+#define MATHLIB_NEURON_H
+
+namespace mathlib {
+
+}  // namespace mathlib
+
+#endif  // MATHLIB_NEURON_H
