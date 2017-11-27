2589bf49d30f4b0e97ecea1e17d82415c473db44
Aleksey Khozin <ahozin@hotmail.com>
2017-11-28 00:26:16 +0300

    Expanded ANN tests.

--- mathlib-test/mathlib-test/nnetwrok_test.cpp
+++ mathlib-test/mathlib-test/nnetwrok_test.cpp
@@ -19,9 +19,15 @@ TEST_F(nnetwork_test_fixture, construct) {
   using map3 = index_pack<2, 0>;
   using Map = type_pack<map1, map2, map3>;
   
-  nnetwork<InputLayer, OutputLayer, Map> network;
-  auto res = network(-1, 0, 1);
-  EXPECT_EQ(std::make_tuple(0, 0, 0), res);
+  using network1_t = nnetwork<InputLayer, OutputLayer, Map>;
+  network1_t network1;
+  auto res1 = network1(-1, 0, 1);
+  EXPECT_EQ(std::make_tuple(0, 0, 0), res1);
+
+  using network2_t = nnetwork<network1_t, std::tuple<Neuron1>, type_pack<index_pack<1, 2, 0>>>;
+  network2_t network2;
+  auto res2 = network2(1, 0, -1);
+  EXPECT_EQ(std::make_tuple(0), res2);
 }
 
 }  // namespace mathlib
