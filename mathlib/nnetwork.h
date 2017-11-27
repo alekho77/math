2eecffc1c634e7192a58e365e28731eb687aee1c
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-11-27 14:27:28 +0300

    Added empty header for ANN definition.

---
+++ mathlib/nnetwork.h
@@ -0,0 +1,15 @@
+/*
+  Object "Neural Network" for artificial neural network modeling.
+  (c) 2017 Aleksey Khozin
+*/
+
+#ifndef MATHLIB_NNETWORK_H
+#define MATHLIB_NNETWORK_H
+
+#include "neuron.h"
+
+namespace mathlib {
+
+}  // namespace mathlib
+
+#endif  // MATHLIB_NNETWORK_H
