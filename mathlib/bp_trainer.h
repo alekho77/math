8b77dcccce23991cf6e41d7acfaacd6b5a2dacbe
Aleksey Khozin <ahozin@hotmail.com>
2017-12-01 23:03:05 +0300

    Cosmetic changes

--- mathlib/bp_trainer.h
+++ mathlib/bp_trainer.h
@@ -16,7 +16,7 @@ namespace mathlib {
 template <typename Network>
 class bp_trainer {
   using value_t = typename Network::value_t;
-  //using apply_t = std::function<value_t ()>;
+  using apply_t = std::function<value_t ()>;
   using input_t = typename make_tuple_type<value_t, Network::input_size>::tuple_type;
 
   static constexpr size_t output_size = std::tuple_size<typename Network::output_t>::value;  // Number of neurons in the output layer.
@@ -24,44 +24,44 @@ class bp_trainer {
 public:
   explicit bp_trainer(Network& net) : network_(net) {}
 
-  void randomize(value_t range = 10, unsigned seed = std::random_device()()) {
+  void randomize(value_t range = 1, unsigned seed = std::random_device()()) {
     std::mt19937 gen(seed);
     std::uniform_real_distribution<value_t> dis(-range, range);
-    pass_layer<Network::num_layers - 1>([&gen, &dis]() { return dis(gen); });
+    pass_layers<Network::num_layers - 1>([&gen, &dis]() { return dis(gen); });
   }
 
   value_t operator ()(const input_t& inputs, const typename Network::output_t& expected_outputs) {
     auto actual_outputs = call_network(inputs, std::make_index_sequence<Network::input_size>());
-    pass_layer<Network::num_layers - 1>([&expected_outputs, &actual_outputs]() { return 0; });
+    pass_layers<Network::num_layers - 1>([&expected_outputs, &actual_outputs]() { return 0; });
     auto fixed_outputs = call_network(inputs, std::make_index_sequence<Network::input_size>());
     return error(expected_outputs, fixed_outputs, std::make_index_sequence<output_size>());
   }
 
 private:
   template <size_t K>
-  void pass_layer(const apply_t& fun) {
+  void pass_layers(const apply_t& fun) {
     using Layer = network_layer_t<K, Network>;
-    pass_neuron<std::tuple_size<Layer>::value - 1, Layer>()(network_.layer<K>(), fun);
-    pass_layer<K - 1>(fun);
+    pass_neurons<std::tuple_size<Layer>::value - 1, Layer>()(network_.layer<K>(), fun);
+    pass_layers<K - 1>(fun);
   }
 
   template <>
-  void pass_layer<0>(const apply_t& fun) {
+  void pass_layers<0>(const apply_t& fun) {
     using Layer = network_layer_t<0, Network>;
-    pass_neuron<std::tuple_size<Layer>::value - 1, Layer>()(network_.layer<0>(), fun);
+    pass_neurons<std::tuple_size<Layer>::value - 1, Layer>()(network_.layer<0>(), fun);
   }
 
   template <size_t I, typename Layer>
-  struct pass_neuron {
+  struct pass_neurons {
     void operator ()(Layer& layer, const apply_t& fun) {
       using Neuron = std::tuple_element_t<I, Layer>;
       apply_neuron<Neuron, Neuron::use_bias>()(std::get<I>(layer), fun);
-      pass_neuron<I - 1, Layer>()(layer, fun);
+      pass_neurons<I - 1, Layer>()(layer, fun);
     }
   };
 
   template <typename Layer>
-  struct pass_neuron<0, Layer> {
+  struct pass_neurons<0, Layer> {
     void operator ()(Layer& layer, const apply_t& fun) {
       using Neuron = std::tuple_element_t<0, Layer>;
       apply_neuron<Neuron, Neuron::use_bias>()(std::get<0>(layer), fun);
