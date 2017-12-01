83efc50cc9bc9e119fc15abab7bc209bacb3abb6
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-12-01 10:15:57 +0300

    Added empty files for bp_trainer object.

---
+++ mathlib-test/mathlib-test/bp_trainer_test.cpp
@@ -0,0 +1,15 @@
+#include "math/mathlib/bp_trainer.h"
+
+#include <gtest/gtest.h>
+
+namespace mathlib {
+
+class bp_trainer_test_fixture : public ::testing::Test {
+protected:
+};
+
+TEST_F(bp_trainer_test_fixture, construct) {
+
+}
+
+}  // namespace mathlib
---
+++ mathlib/bp_trainer.h
@@ -0,0 +1,17 @@
+/*
+  Back propagation trainer for artificial neural network.
+  (c) 2017 Aleksey Khozin
+*/
+
+#ifndef MATHLIB_BP_TRAINER_H
+#define MATHLIB_BP_TRAINER_H
+
+#include "helpers.h"
+
+#include <cmath>
+
+namespace mathlib {
+
+}  // namespace mathlib
+
+#endif  // MATHLIB_BP_TRAINER_H
