449fd55f872cbf419a6b40d397e6ea7518b3e6bc
Aleksey Khozin <ahozin@hotmail.com>
2017-12-02 14:19:40 +0300

    Sketches of forward pass that is a part of back propagation algorithm.

--- mathlib/bp_trainer.h
+++ mathlib/bp_trainer.h
@@ -21,6 +21,11 @@ class bp_trainer {
 
   static constexpr size_t output_size = std::tuple_size<typename Network::output_t>::value;  // Number of neurons in the output layer.
 
+  template <size_t... I>
+  static auto helper_network_data(std::index_sequence<I...>) -> decltype(
+    std::make_tuple(typename make_tuple_type<value_t, std::tuple_size<network_layer_t<I, Network>>::value>::tuple_type()...)) {}
+  using network_data_t = decltype(helper_network_data(std::make_index_sequence<Network::num_layers>()));
+
 public:
   explicit bp_trainer(Network& net) : network_(net) {}
 
@@ -31,8 +36,11 @@ public:
   }
 
   value_t operator ()(const input_t& inputs, const typename Network::output_t& expected_outputs) {
+    // Forward pass to determine values of all neurons.
+    network_data_t neurons_values = forward_pass(inputs);
+    
     auto actual_outputs = call_network(inputs, std::make_index_sequence<Network::input_size>());
-    pass_layers<Network::num_layers - 1>([&expected_outputs, &actual_outputs]() { return 0; });
+    //pass_layers<Network::num_layers - 1>([&expected_outputs, &actual_outputs]() { return 0; });
     auto fixed_outputs = call_network(inputs, std::make_index_sequence<Network::input_size>());
     return error(expected_outputs, fixed_outputs, std::make_index_sequence<output_size>());
   }
@@ -102,6 +110,42 @@ private:
       return fun();
     }
   };
+
+  network_data_t forward_pass(const input_t& inputs) {
+    network_data_t neuron_values;
+    call_layer<0>(inputs, neuron_values);
+    return neuron_values;
+  }
+
+  template <size_t I>
+  struct layer_input {
+    using type = std::tuple_element_t<I - 1, network_data_t>;
+  };
+  template <>
+  struct layer_input<0> {
+    using type = input_t;
+  };
+
+  template <size_t I>
+  void call_layer(typename const layer_input<I>::type& /*inputs*/, network_data_t& data) {
+    //std::get<I>(data) = net_.layer<I>();
+    call_layer<I + 1>(std::get<I>(data), data);
+  }
+
+  template <>
+  void call_layer<Network::num_layers - 1>(typename const layer_input<Network::num_layers - 1>::type& /*inputs*/, network_data_t& /*data*/) {
+    //std::get<Network::num_layers - 1>(data) = net_.layer<Network::num_layers - 1>();
+  }
+
+  template <size_t K, size_t... I>
+  std::tuple_element_t<K, network_data_t> map_neurons(const network_layer_t<K, Network>& layer, std::index_sequence<I...>) {
+    std::forward_as_tuple(call_neuron<>(std::get<I>(layer),  std::make_index_sequence<std::tuple_size<network_layer_t<K, Network>>::value>())...);
+  }
+
+  template <typename Indexes, typename Neuron, typename Input, size_t... J>
+  value_t call_neuron(const Neuron& n, const Input& inputs, std::index_sequence<J...>) const {
+    return n(std::get<get_index<J, Indexes>()>(inputs)...);
+  }
 
   template <size_t... I>
   inline typename Network::output_t call_network(const input_t& inputs, std::index_sequence<I...>) {
