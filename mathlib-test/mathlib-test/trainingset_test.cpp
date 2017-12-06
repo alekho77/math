cdb78d5c7bda3fa8404e602dbea986e281d07db8
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-12-06 21:07:13 +0300

    Implemented training set for ANN.

--- mathlib-test/mathlib-test/trainingset_test.cpp
+++ mathlib-test/mathlib-test/trainingset_test.cpp
@@ -10,6 +10,14 @@ namespace mathlib {
 
 class trainingset_test_fixture : public ::testing::Test {
 protected:
+  void SetUp() override {
+    auto& layer1 = network.layer<0>();
+    std::get<0>(layer1).set_weights(0.45, -0.12);
+    std::get<1>(layer1).set_weights(0.78, 0.13);
+    auto& layer2 = network.layer<1>();
+    std::get<0>(layer2).set_weights(1.5, -2.3);
+  }
+
   using InputLayer = input_layer<double, 2>;
   using Neuron1 = neuron<double, 2>;
   using Neuron2 = neuron<double, 2, NOBIAS<double>>;
@@ -28,6 +36,12 @@ protected:
     std::make_tuple(std::make_tuple(0.0, 1.0), 1.0),
     std::make_tuple(std::make_tuple(1.0, 1.0), 0.0)
   };
+  const std::vector<std::tuple<double, double>> errors = {
+    std::make_tuple(0.0, 0.0),
+    std::make_tuple(1.575851325117215, 0.5151895873080313),
+    std::make_tuple(0.8321630367385499, 0.223760066502746),
+    std::make_tuple(0.4117459150788285, 0.332333996443997)
+  };
 };
 
 TEST_F(trainingset_test_fixture, load) {
@@ -37,6 +51,26 @@ TEST_F(trainingset_test_fixture, load) {
   auto training = make_training_set<bp_trainer>(network);
   size_t count = training.load(in);
   EXPECT_EQ(4, count);
+}
+
+TEST_F(trainingset_test_fixture, training) {
+  using namespace boost::iostreams;
+  stream_buffer<array_source> buf(reinterpret_cast<const char*>(samples.data()), sizeof(Sample) * samples.size());
+  std::istream in(&buf);
+  auto training = make_training_set<bp_trainer>(network);
+  ASSERT_EQ(4, training.load(in));
+  size_t count = 0;
+  auto cb = [&count, this](size_t idx, auto inputs, auto outputs, auto errs) {
+    EXPECT_EQ(count, idx);
+    EXPECT_EQ(std::get<0>(this->samples[idx]), inputs);
+    EXPECT_EQ(std::get<1>(this->samples[idx]), std::get<0>(outputs));
+    EXPECT_DOUBLE_EQ(std::get<0>(this->errors[idx]), std::get<0>(errs));
+    EXPECT_DOUBLE_EQ(std::get<1>(this->errors[idx]), std::get<1>(errs));
+    count++;
+  };
+  training.set_learning_rate(0.7);
+  training.set_moment(0.3);
+  training(cb);
 }
 
 }  // namespace mathlib
--- mathlib/trainingset.h
+++ mathlib/trainingset.h
@@ -10,6 +10,8 @@
 
 #include <istream>
 #include <vector>
+#include <random>
+#include <algorithm>
 
 namespace mathlib {
 
@@ -20,6 +22,7 @@ class training_set {
   using input_t = typename trainer_t::input_t;
   using output_t = typename Network::output_t;
   using sample_t = std::tuple<input_t, output_t>;
+  using errors_t = std::tuple<value_t, value_t>;
 
   static_assert(sizeof(sample_t) == (std::tuple_size<input_t>::value + std::tuple_size<output_t>::value) * sizeof(value_t), "Something wrong with type size or alignment.");
 
@@ -37,15 +40,45 @@ public:
     }
     if (in.eof()) {
       samples_.swap(samples);
+      sort();
       return samples_.size();
     }
     return 0;
+  }
+
+  void sort() {
+    std::vector<size_t> indexes;
+    indexes.reserve(samples_.size());
+    for (size_t i = 0; i < samples_.size(); i++) {
+      indexes.emplace_back(i);
+    }
+    indexes_.swap(indexes);
+  }
+
+  void shuffle(unsigned seed = std::random_device()()) {
+    std::mt19937 gen(seed);
+    std::shuffle(indexes_.begin(), indexes_.end(), gen);
+  }
+
+  value_t learning_rate() const { return trainer_.learning_rate(); }
+  void set_learning_rate(value_t eta) { trainer_.set_learning_rate(eta); }
+
+  value_t moment() const { return trainer_.moment(); }
+  void set_moment(value_t alpha) { trainer_.set_moment(alpha); }
+
+  template <typename Callback>
+  void operator ()(const Callback& cb) {
+    for (size_t idx: indexes_) {
+      errors_t errs = trainer_(std::get<0>(samples_[idx]), std::get<1>(samples_[idx]));
+      cb(idx, std::get<0>(samples_[idx]), std::get<1>(samples_[idx]), errs);
+    }
   }
 
 private:
   Network& network_;
   trainer_t trainer_;
   std::vector<sample_t> samples_;
+  std::vector<size_t> indexes_;
 };
 
 template <template <typename> class Trainer, typename Network>
