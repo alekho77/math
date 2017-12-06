fff6bcfa5eaa8988398aa146a1fcc427cec4d2db
Aleksey Khozin <Aleksey.Khozin@transas.com>
2017-12-06 18:32:07 +0300

    Added empty header for training set object

---
+++ mathlib/trainingset.h
@@ -0,0 +1,15 @@
+/*
+  Training set for learning of artificial neural network.
+  (c) 2017 Aleksey Khozin
+*/
+
+#ifndef MATHLIB_TRAININGSET_H
+#define MATHLIB_TRAININGSET_H
+
+#include "nnetwork.h"
+
+namespace mathlib {
+
+}  // namespace mathlib
+
+#endif  // MATHLIB_TRAININGSET_H
