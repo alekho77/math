65aaacf8d8ea14874cba59a044371583c73909f5
Aleksey Khozin <ahozin@hotmail.com>
2017-12-27 18:30:16 +0300

    First sketch of CNN

--- mathlib/cnnetwork.h
+++ mathlib/cnnetwork.h
@@ -6,7 +6,31 @@
 #ifndef MATHLIB_CNNETWORK_H
 #define MATHLIB_CNNETWORK_H
 
+#include <memory>
+
 namespace mathlib {
+
+enum class cnfunction {
+  sigmoid
+};
+
+struct cnneuron {
+  cnfunction type;
+  size_t synapses;
+  bool bias;
+};
+
+class cnnetwork {
+public:
+  cnnetwork(cnnetwork&&) = default;
+  ~cnnetwork() = default;
+
+  static cnnetwork make(size_t inputs, );
+private:
+  cnnetwork();
+  class impl;
+  std::unique_ptr<impl> impl_;
+};
 
 }  // namespace mathlib
 
